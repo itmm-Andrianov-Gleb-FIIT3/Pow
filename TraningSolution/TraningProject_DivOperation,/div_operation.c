30c4881f55a878c969d3ea227b579e5d913e0145
itmm-Andrianov-Gleb-FIIT3 <glebius.gleb@yandex.ru>
2022-10-06 19:31:56 +0300

    исправлена ошибка с выведением -0

--- TraningSolution/TraningProject_DivOperation,/div_operation.c
+++ TraningSolution/TraningProject_DivOperation,/div_operation.c
@@ -3,8 +3,12 @@ int main() {
    int a, b;
    float div;
    scanf_s("%d %d", &a, &b);
-   div = a / b;
-   printf("%d", div);
+   div = ((float)(a) / (b));
+   printf("%.2f\n", div);
+   if (div == -0) {
+	   system("cls");
+	   printf("0");
+   }
    system("pause");
    return 0;
 }
