d9c851524e6fe3a7d76dfac505b9d4cf7c17361d
itmm-Andrianov-Gleb-FIIT3 <glebius.gleb@yandex.ru>
2022-10-06 19:46:18 +0300

    Финальная версия

--- TraningSolution/TraningProject_DivOperation,/div_operation.c
+++ TraningSolution/TraningProject_DivOperation,/div_operation.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
+#include <locale.h>
 int main() {
+	setlocale(LC_ALL,"rus");
    int a, b;
    float div;
+   printf("¬ведите a и b: ");
    scanf_s("%d %d", &a, &b);
-   div = a / b;
-   printf("%d", div);
+   div = ((float)(a) / (b));
+   system("cls");
+   printf("%.2f\n", div);
+   if (div == -0) {
+	   system("cls");
+	   printf("0\n");
+   }
    system("pause");
    return 0;
 }
