602a2b36f06451914ae3bd851981e961c02031fa
itmm-Andrianov-Gleb-FIIT3 <glebius.gleb@yandex.ru>
2022-10-06 19:41:45 +0300

    Финальная версия

--- TraningSolution/TraningProject_DivOperation,/div_operation.c
+++ TraningSolution/TraningProject_DivOperation,/div_operation.c
@@ -3,12 +3,8 @@ int main() {
    int a, b;
    float div;
    scanf_s("%d %d", &a, &b);
-   div = ((float)(a) / (b));
-   printf("%.2f\n", div);
-   if (div == -0) {
-	   system("cls");
-	   printf("0");
-   }
+   div = a / b;
+   printf("%d", div);
    system("pause");
    return 0;
 }
